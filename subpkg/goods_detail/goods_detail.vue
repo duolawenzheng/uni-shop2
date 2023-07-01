7d85a2321ddae1e4d6ae8f8a73606a1668442552
哆啦文正 <8515686+dora-wenzheng@user.noreply.gitee.com>
2023-07-01 21:36:51 +0800

    完成了商品详情页面的开发

--- subpkg/goods_detail/goods_detail.vue
+++ subpkg/goods_detail/goods_detail.vue
@@ -1,6 +1,35 @@
 <template>
-  <view>
+  <view v-if="goods_info.goods_name" class="good-detail-container">
+    <!-- 轮播图区域 -->
+    <swiper :indicator-dots="true" :autoplay="true" :interval="3000" :duration="1000" :circular="true">
+      <swiper-item v-for="(item,i) in goods_info.pics" :key="i">
+        <image :src="item.pics_big" @click="preview(i)"></image>
+      </swiper-item>
+    </swiper>
+    <!-- 商品信息区域 -->
+    <view class="goods-info-box">
+      <!-- 商品价格 -->
+      <view class="price">{{goods_info.goods_price}}</view>
+      <!-- 商品信息的主题区域 -->
+      <view class=" goods-info-body">
+        <!-- 商品的名字 -->
+        <view class="goods-name">{{goods_info.goods_name}}</view>
+        <!-- 收藏 -->
+        <view class="favi">
+          <uni-icons type="star" size="18" color="gray"></uni-icons>
+          <text>收藏</text>
+        </view>
+      </view>
+      <!-- 运费 -->
+      <view class="yf">快速：免运费</view>
+    </view>
     
+    <rich-text :nodes="goods_info.goods_introduce"></rich-text>
+    
+    <!-- 商品导航组件区域 -->
+    <view class="goods_nav">
+      <uni-goods-nav :fill="true"  :options="options" :buttonGroup="buttonGroup"  @click="onClick" @buttonClick="buttonClick" />
+    </view>
   </view>
 </template>
 
@@ -8,12 +37,119 @@
   export default {
     data() {
       return {
-        
+        goods_info:{},
+        options: [{
+        			icon: 'headphones',
+        			text: '客服'
+        		}, {
+        			icon: 'shop',
+        			text: '店铺',
+        			info: 2,
+        			infoBackgroundColor:'#007aff',
+        			infoColor:"red"
+        		}, {
+        			icon: 'cart',
+        			text: '购物车',
+        			info: 2
+        		}],
+        	    buttonGroup: [{
+        	      text: '加入购物车',
+        	      backgroundColor: '#ff0000',
+        	      color: '#fff'
+        	    },
+        	    {
+        	      text: '立即购买',
+        	      backgroundColor: '#ffa200',
+        	      color: '#fff'
+        	    }
+        	    ]
       };
+    },
+    onLoad(options) {
+      console.log(options);
+      const goods_id = options.goods_id
+      this.getGoodsDetail(goods_id)
+    },
+    methods:{ 
+      async getGoodsDetail(goods_id){
+        const {data:res} = await uni.$http.get('api/public/v1/goods/detail/',{ goods_id })
+        // console.log(res);
+        if(res.meta.status!==200) return uni.$showMsg() 
+        
+        res.message.goods_introduce=res.message.goods_introduce.replace(/<img /g,'<img style="display:block;"').replace(/webp/g,'jpg')
+        this.goods_info=res.message
+      },
+      preview(i){
+        // 调用previewImage方法预览图片
+        uni.previewImage({
+          // 预览时候默认显示图片索引
+          current:i,
+          // 将所有图片的URL地址封进数组
+          urls:this.goods_info.pics.map(x=>x.pics_big)
+        })
+      },
+      onClick(e){
+        console.log(e);
+        if(e.content.text==="购物车"){
+          uni.switchTab({
+            url:'/pages/cart/cart'
+          })
+        }
+      }
     }
-  }
-</script>
+  }      
+</script> 
 
 <style lang="scss">
-
+swiper{
+  height: 750rpx;
+  image{
+    height: 100%;
+    width: 100%;
+  }
+}
+.goods-info-box{
+  padding: 10px;
+  padding-right: 0;
+  .price{
+    color: #C00000;
+    font-size: 18px;
+    margin: 10px 0;
+  }
+  .goods-info-body{
+    display: flex;
+    justify-content: space-between;
+    .goods-name{
+      font-size: 13px;
+      margin-right: 10px;
+    }
+    .favi{
+      width: 120px;
+      font-size: 12px;
+      display: flex;
+      flex-direction: column;
+      // 横向和纵向居中
+      align-items: center;
+      justify-content: center;
+      border-left: 1px solid #efefef;
+      color: gray;
+    }
+    
+  }
+  .yf{
+    font-size: 12px;
+    color: grey;
+    margin: 10px 0;
+  }
+}
+.goods_nav{
+  // 固定在页面底部 不随滚动而变化位置
+  position:fixed;
+  bottom: 0;
+  left: 0;
+  width: 100%;
+}
+.good-detail-container{
+  padding-bottom: 50px;
+}
 </style>
